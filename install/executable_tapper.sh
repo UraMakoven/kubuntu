d39d10ded8578e6e8ab5e7e3b7708283f6b8a133
ura.home <ura.makoven@gmail.com>
2025-11-26 15:04:05 +0200

    Add install/tapper.sh

---
+++ install/executable_tapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/install/src
+
+tar xzf tapper-0.7.4-0.vdb.1.tar.gz
+cd tapper-0.7.4-0.vdb.1
+
+mkdir _build
+cd _build
+
+sudo ../bin/setup.sh -y
+
+../configure
+make -j
+
+sudo make install
+
+#29:KEY_LEFTCTRL, 54:KEY_RIGHTSHIFT, 97:KEY_RIGHTCTRL
+tapper --save-setting 29=@1, 97=@2, 54=@3
+
+tapper --autostart
